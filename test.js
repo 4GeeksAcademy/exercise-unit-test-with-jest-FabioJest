5e90664fa7cf8c994c314b1647f5ed9c0c00164a
Fabio Elías Rengifo García <81293357+RengifoGarciaFE@users.noreply.github.com>
2025-12-05 11:29:54 +0000

    cambios hechos

--- test.js
+++ test.js
@@ -1,43 +1,25 @@
 const { fromDollarToYen, fromEuroToDollar, fromYenToPound } = require('./app.js');
 
 test("One euro should be 1.07 dollars", function() {
-    // Importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js');
-
-    // Uso la función como debe ser usada
     const dollars = fromEuroToDollar(3.5);
-
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
     const expected = 3.5 * 1.07;
+    expect(dollars).toBe(expected);
+});
 
-    // Hago mi comparación (la prueba)
-    expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
-})
-
-test("One dollar should be 146.26 yens", function() {
-    // Importo la funcion desde app.js
-    const { fromDollarToYen } = require('./app.js');
-
-    // Uso la función como debe ser usada
+test("One dollar should be correctly converted to yen", function() {
     const yens = fromDollarToYen(3.5);
 
-    // Si 1 dollar son 146.26 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 146.26;
+    // cálculo esperado paso por paso
+    const expected = (3.5 / 1.07) * 156.5;
 
-    // Hago mi comparación (la prueba)
-    expect(yens).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
-})
+    expect(yens).toBe(expected);
+});
 
-test("One yen should be 0.00555910543 pounds", function() {
-    // Importo la funcion desde app.js
-    const { fromYenToPound } = require('./app.js');
-
-    // Uso la función como debe ser usada
+test("One yen should be correctly converted to pounds", function() {
     const pounds = fromYenToPound(3.5);
 
-    // Si 1 yen son 0.00555910543 pounds
-    const expected = 3.5 * 0.00555910543;
+    // cálculo esperado paso por paso
+    const expected = (3.5 / 156.5) * 0.87;
 
-    // Hago mi comparación (la prueba)
-    expect(pounds).toBe(expected); 
-})
+    expect(pounds).toBe(expected);
+});
